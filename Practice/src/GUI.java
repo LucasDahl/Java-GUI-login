4e392bab9180671e5e7c740378089e8742f9829d
Lucas Dahl <LucasDahl425@yahoo.com>
2020-05-01 15:35:07 -0700

    Completed the GUI by adding a button and a label that records the number of clicks.

--- Practice/src/GUI.java
+++ Practice/src/GUI.java
@@ -1,6 +1,58 @@
-public class GUI {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+public class GUI implements ActionListener {
+
+    // Properties
+    private int count = 0;
+    private JLabel clickLabel;
+    private JFrame frame;
+    private JPanel panel;
+
+    // GUI method.
+    public GUI() {
+
+        // Create JFrame objects
+        frame = new JFrame();
+        panel = new JPanel();
+
+        // Create UI elements
+        JButton countButton = new JButton("Click on me!!!");
+        countButton.addActionListener(this);
+        clickLabel = new JLabel("Number of clicks: 0");
+
+        // Setup the panel
+        panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
+        panel.setLayout(new GridLayout(0, 1));
+        panel.add(countButton);
+        panel.add(clickLabel);
+
+        // Setup the frame
+        frame.add(panel, BorderLayout.CENTER);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("GUI");
+        frame.pack();
+        frame.setVisible(true);
+
+
+    }
+
+    // Main method for running
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        new GUI();
+    }
+
+    // THis is for actions, in this case for the button.
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+        // Increment the count per click.
+        count ++;
+
+        // Update the label
+        clickLabel.setText("Number of clicks: " + count);
+
     }
 }
